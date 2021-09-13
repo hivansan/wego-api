f7311fcd0ef4416ffec3510107dcde278c1fb91e
Ivan Flores <hivansan@gmail.com>
2021-09-13 13:12:47 -0500

    asset get assets from collection

--- common/models/asset.js
+++ common/models/asset.js
@@ -1,4 +1,5 @@
 'use strict';
+const axios = require('axios');
 const { arrayFetch } = require('../../lib/fetchNParse');
 
 module.exports = (Asset) => {
@@ -47,6 +48,62 @@ module.exports = (Asset) => {
         ),
       });
       return asset;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  Asset.remoteMethod('getAssetsFromCollection', {
+    http: {
+      verb: 'get',
+    },
+    accepts: [
+      { arg: 'contractAddress', type: 'string', required: true },
+      { arg: 'tokenId', type: 'string', required: false },
+    ],
+    returns: {
+      type: 'object',
+      root: true,
+    },
+  });
+
+  Asset.getAssetsFromCollection = async (contractAddress, tokenId) => {
+    //   curl --request GET \
+    //  --url 'https://api.opensea.io/api/v1/assets?token_ids=9974&asset_contract_address=0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d&order_direction=desc&offset=0&limit=20'
+
+    //  --url 'https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=1&collection=infinites-ai'
+
+    try {
+      let page = 0;
+      let assets = [];
+      const params = {
+        asset_contract_address: contractAddress,
+        offset: 0,
+        limit: 50,
+      };
+
+      if (tokenId) params.token_ids = tokenId;
+      let queryParams = new URLSearchParams(params).toString();
+      let url = `https://api.opensea.io/api/v1/assets?${queryParams}`;
+      let results = await axios(url);
+      assets = results?.data?.assets;
+      delete params.token_ids;
+      while (results.data?.assets?.length) {
+        page++;
+        params.offset = page * params.limit;
+        queryParams = new URLSearchParams(params).toString();
+        url = `https://api.opensea.io/api/v1/assets?${queryParams}`;
+        results = await axios(url);
+        console.log(
+          'page, asset',
+          page,
+          contractAddress,
+          results?.data?.assets?.length
+        );
+        if (results?.data?.assets?.length)
+          assets = [...assets, results?.data?.assets];
+      }
+      return assets;
     } catch (error) {
       throw error;
     }
