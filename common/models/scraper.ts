6db4ccef803b61da25ca098ce221ff220fd12ecf
Ivan Flores <hivansan@gmail.com>
2021-10-01 01:22:00 -0500

    scrape assets from collection

--- common/models/scraper.ts
+++ common/models/scraper.ts
@@ -1,100 +1,114 @@
 'use strict';
 
-import { getAbi, getWeb3, saveAbis, saveCollections, saveSupply, saveTokensUri } from '../../lib/scraper.helpers';
+import fs from 'fs';
+import path from 'path';
 
-export default (Scraper) => {
-  Scraper.remoteMethod('getTokenIdJSON', {
-    accepts: [
-      {
-        arg: 'address',
-        type: 'string',
-        required: false,
-      },
-      {
-        arg: 'tokenId',
-        type: 'string',
-        required: false,
-      },
-    ],
-    returns: {
-      type: 'object',
-      root: true,
-    },
-  });
+import { Client } from '@elastic/elasticsearch';
+import moment from 'moment';
+import axios, { AxiosRequestConfig } from 'axios';
 
-  Scraper.getTokenIdJSON = async (address, tokenId) => {
-    return getWeb3({
-      address,
-      methodParams: tokenId,
-      method: 'tokenURI',
-    });
-  };
+import * as QuerySQL from '../../lib/query.mysql';
+import { URLSearchParams } from 'url';
+import { fromPairs, map, pipe, props } from 'ramda';
+const client = new Client({ node: 'http://localhost:9200', requestTimeout: 1000 * 60 * 60 });
 
-  Scraper.remoteMethod('getAbi', {
-    accepts: [
-      {
-        arg: 'address',
-        type: 'string',
-        required: true,
-      },
-    ],
-    returns: {
-      type: 'object',
-      root: true,
-    },
-  });
-
-  // this also saves the abi if it doesn't exist
-  Scraper.getAbi = getAbi;
-
-  Scraper.remoteMethod('saveAbis', {
-    accepts: [],
-    returns: {
-      type: 'object',
-      root: true,
-    },
-  });
-
-  Scraper.saveAbis = saveAbis;
-
-  Scraper.remoteMethod('saveSupply', {
-    accepts: [],
-    returns: {
-      type: 'object',
-      root: true,
-    },
-  });
-
-  Scraper.saveSupply = saveSupply;
-
-  Scraper.remoteMethod('saveTokensUri', {
-    accepts: [],
-    returns: {
-      type: 'object',
-      root: true,
-    },
-  });
-
-  Scraper.saveTokensUri = saveTokensUri;
-
-  Scraper.remoteMethod('saveCollections', {
-    accepts: [],
-    returns: {
-      type: 'object',
-      root: true,
-    },
-  });
-
-  Scraper.saveCollections = saveCollections;
+const bail = (err) => {
+  console.error(err);
+  process.exit(1);
 };
 
-// const Web3 = require("web3")
-// const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/YOUR_PROJECT_ID"))
-// get the balance of an ethereum address
-// web3.eth.getBalance("0x5A0b54D5dc17e0AadC383d2db43B0a0D3E029c4c", function(err, result) {
-//   if (err) {
-//     console.log(err)
-//   } else {
-//     console.log(web3.utils.fromWei(result, "ether") + " ETH")
-//   }
-// })
+const slugName: string | undefined = process.argv.find((s) => s.startsWith('--slug='))?.replace('--slug=', '');
+
+const xForm = pipe(
+  map(props(['trait_type', 'value'])),
+  fromPairs as any
+)
+
+
+const openseaAssetMapper = (asset: any) => ({
+  tokenId: asset.token_id,
+  contractAddress: asset.asset_contract.address,
+  slug: asset.collection.slug,
+  name: asset.name,
+  owners: asset.owners,
+  owner: asset.owner,
+  description: asset.description, //  rariMeta.description,
+  imageBig: asset.image_original_url, // rariMeta.image.url.BIG,
+  imageSmall: asset.image_preview_url, // rariMeta.image.url.PREVIEW,
+  animationUrl: asset.animation_url,
+  traits: asset.traits,
+  traitMap: xForm(asset.traits),
+  //rariscore: https://raritytools.medium.com/ranking-rarity-understanding-rarity-calculation-methods-86ceaeb9b98c
+  rariScore:
+    asset?.traits?.length && asset.collection?.stats?.total_supply
+      ? asset.traits.reduce(
+          (acc, t) =>
+            acc + 1 / (t.trait_count / asset.collection.stats.total_supply),
+          0
+        )
+      : null,
+  tokenMetadata: asset.token_metadata,
+  updatedAt: new Date(),
+});
+
+
+const saveAssetsFromCollection = async (slug?: string) => {
+  try {
+    // const { Collection, Asset } = Scraper.app.models;
+    const collection = await QuerySQL.findOne(`select * from Collection where slug = '${slug}' limit 1`)
+   
+    if (!collection?.slug) return;
+        
+    const today = moment();      
+    const updatedDate = moment(collection.updatedAt);
+    const shouldUpdate = !collection.updatedAt || today.diff(updatedDate, 'days') > 3;
+      // assetsCount != null &&
+      // collection.totalSupply &&
+      // assetsCount < collection.totalSupply;
+
+    console.log(`slug: ${slug} assetsCount: completed: ${!shouldUpdate}`);
+    if (!shouldUpdate) {
+      return;
+    }
+
+    let page = 0;
+    let assets: Array<any> = [];
+    const params: any = {
+      collection: slug,
+      offset: 0,
+      limit: 50,
+    };
+
+    let queryParams: URLSearchParams, url: any;
+    let results = { data: { assets: [1] } };
+    while (results.data?.assets?.length && page < 201) {
+      params.offset = page * params.limit;
+      queryParams = new URLSearchParams(params); //.toString();
+      url = `https://api.opensea.io/api/v1/assets?${queryParams}`;
+      console.log(`page: ${page} slug: ${slug} offset: ${params.offset} results?.data?.assets.length: ${results?.data?.assets.length}`);
+      console.log(`page: ${url}`);
+      results = await axios(url);
+
+
+      if (results?.data?.assets?.length) {
+        console.log('results?.data?.assets?.length',results?.data?.assets?.length);
+        assets = [...assets, ...results?.data?.assets];
+        // scraperHelpers.saveAssets(results.data.assets);
+        page++;
+      }
+    }
+
+    // console.log(JSON.stringify(assets.map(openseaAssetMapper)));
+    // writeFilePromisse(`./data/${slug}.json`, 'JSON.stringify(assets.map(openseaAssetMapper))');
+    fs.writeFile(`./data/${slug}.json`, JSON.stringify(assets.map(openseaAssetMapper)), (err) => {
+      if (err) console.log(`[write file err] ${err}`);
+    });
+    QuerySQL.run(`update Collection set updatedAt = '${moment().format("YYYY-MM-DD HH:mm:ss")}' where slug = '${slug}'`)
+    
+    return {}
+  } catch (error) {
+    throw error;
+  }
+};
+
+saveAssetsFromCollection(slugName);
---
+++ lib/query.mysql.ts
@@ -0,0 +1,25 @@
+import mysql from 'mysql';
+import { curry } from 'ramda';
+
+import * as datasources from '../server/datasources.json';
+const { mysqlDs } = datasources;
+
+/**
+ * @param {String} - q : query
+ * @param {String} - queryType: findOne | find | update | create
+ */
+export const run = (q: string, queryType?: string): Promise<any> =>
+  new Promise((resolve, reject) => {
+    const con = mysql.createConnection(mysqlDs);
+    con.connect((err) => {
+      if (err) throw err;
+      // q = 'select * from Collection;'
+      con.query(q, (err, results, fields) => {
+        con.end();
+        if (results?.length) resolve(results);
+        if (err) reject(err);
+      });
+    });
+  });
+
+export const findOne = async (q) => (await run(q))[0];
