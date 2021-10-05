c5ca43f9dde2294d296e1569c0ee09d40fb9479a
Ivan Flores <hivansan@gmail.com>
2021-10-05 17:59:54 -0500

    scraper git ignore

--- common/models/scraper.ts
+++ common/models/scraper.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 'use strict';
 
 import fs from 'fs';
@@ -9,7 +10,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 
 import * as QuerySQL from '../../lib/query.mysql';
 import { URLSearchParams } from 'url';
-import { curry, fromPairs, map, pick, pipe, prop, props, descend, sortBy, sortWith } from 'ramda';
+import { curry, fromPairs, map, pick, pipe, prop, props, descend, sortBy, sortWith, tap, flatten, dropRepeats, forEachObjIndexed, forEach } from 'ramda';
 const client = new Client({ node: 'http://localhost:9200', requestTimeout: 1000 * 60 * 60 });
 
 const bail = (err) => {
@@ -19,11 +20,14 @@ const bail = (err) => {
 
 const slugName: string | undefined = process.argv.find((s) => s.startsWith('--slug='))?.replace('--slug=', '');
 const exec: string | undefined = process.argv.find((s) => s.startsWith('--exec='))?.replace('--exec=', '');
+const bots: any = process.argv.find((s) => s.startsWith('--bots='))?.replace('--bots=', '');
 
 const xForm = pipe(
   map(props(['trait_type', 'value'])),
   fromPairs as any
 )
+
+const DAYS_WINDOW = 5;
 
 const openseaAssetMapper = (asset: any) => ({
   tokenId: asset.token_id,
@@ -52,10 +56,13 @@ const openseaAssetMapper = (asset: any) => ({
 });
 
 
+/**
+ * maybe not used anymore
+ */
 export const saveAssetsFromCollection = async (slug?: string) => {
   try {
     // const { Collection, Asset } = Scraper.app.models;
-    const collection = await QuerySQL.findOne(`select * from Collection where slug is not null and slug = '${slug}' and updatedAt < '${moment().subtract(3, "days").format("YYYY-MM-DD HH:mm:ss")}' limit 1`)
+    const collection = await QuerySQL.findOne(`select * from Collection where slug is not null and slug = '${slug}' and updatedAt < '${moment().subtract(DAYS_WINDOW, "days").format("YYYY-MM-DD HH:mm:ss")}' limit 1`)
 
     const today = moment();
     const updatedDate = moment(collection.updatedAt);
@@ -103,30 +110,82 @@ export const saveAssetsFromCollection = async (slug?: string) => {
     });
     QuerySQL.run(`update Collection set updatedAt = '${moment().format("YYYY-MM-DD HH:mm:ss")}' where slug = '${slug}'`)
 
-    return {}
+    // return {}
   } catch (error) {
     // throw error;
     return
   }
 };
 
+export const saveAssetsFromLinks = async (links: string[]): Promise<void> => {
+  for (const url of links) {
+    try {
+      const results = await axios(url);
+
+      const assets = await results?.data?.assets;
+      if (assets?.length) {
+        const slug = (url as any).split('&').find((s: string) => s.startsWith('collection=')).split('=')[1]
+        const offset = (url as any).split('&').find((s: string) => s.startsWith('offset=')).split('=')[1]
+
+        // console.log(`[${botNo}]`, url, results.data?.assets?.length, slug, offset);
+        console.log(url, results.data?.assets?.length, slug, offset);
+
+        fs.writeFile(`./data/chunks/${slug}:${offset}.json`, JSON.stringify(assets.map(openseaAssetMapper)), (err) => {
+          if (err) console.log(`[write file err] ${err}`);
+        });
+
+        if (assets.length < 50){
+          QuerySQL.run(`update Collection set updatedAt = '${moment().format("YYYY-MM-DD HH:mm:ss")}' where slug = '${slug}'`)
+        }
+      }
+
+    } catch (e) {
+      console.log(`[err] ${e} ${url}`);
+      fs.appendFile('./data/url-504.txt', url, (err) => {
+        if (err) console.log(`[save 504 file error]`, err);
+      });
+    }
+  }
+}
+
+const getLinks = (c: { totalSupply: number; slug: any }): string[] =>
+  [...Array(Math.ceil(c.totalSupply / 50)).keys()].map((i) =>
+    `https://api.opensea.io/api/v1/assets?limit=50&offset=${i * 50}&collection=${c.slug}`);
+
+const topSupply = map((c) => ({ ...Object(c), totalSupply: Object(c).totalSupply > 10000 ? 10000 : Object(c).totalSupply }));
+const toLinks = map((c): string[] => [...getLinks(Object(c))]);
+
+const sendToBots = (links: string[]) => {
+  const chunkLength = Math.ceil(links.length / bots);
+  for (let i = 0; i < +bots; i++) saveAssetsFromLinks(links.splice(0, chunkLength), /* i */);
+};
+
+const getSplices = (links: string[]): string[][]=> {
+  const len = Math.ceil(links.length/+bots);  
+  return [...Array(+bots).keys()].map((c, i) => links.splice(0, len))
+}
+
 export const saveAssetsFromCollections = () =>
-  QuerySQL.find(`select * from Collection where updatedAt < '${moment().subtract(3, "days").format("YYYY-MM-DD HH:mm:ss")}';`)
-    // .then(map(Object))
+  QuerySQL.find(`select * from Collection where updatedAt < '${moment().subtract(DAYS_WINDOW, 'days').format('YYYY-MM-DD HH:mm:ss')}';`)
     // .then(sortWith(descend(prop('totalSupply'))))
     .then(map(pick(['slug', 'totalSupply'])))
+
+    // .then(pipe(sortBy(prop('totalSupply')), map(Object), topSupply, toLinks, flatten, dropRepeats, getSplices))
+
     .then(sortBy(prop('totalSupply')))
-    .then(collections => collections.reverse())
-    .then(async (collections: any) => {
-      for (const collection of collections) {
-        console.log(`working on ${collection.slug} colle totalsupply: ${collection.totalSupply}`);
-        await saveAssetsFromCollection(collection.slug)
-      }
-    })
-    // for mass
-    // .then(collections => Promise.all(collections.map(pipe(prop('slug'), saveAssetsFromCollection))))
-    .catch(e => {
+    .then(map(Object))
+    .then(topSupply)
+    .then(toLinks)
+    .then(flatten)
+    .then(dropRepeats)
+    .then(tap((x) => console.log(x.length)))
+    // .then(sendToBots)
+    .then(getSplices)
+    .then(tap((x) => console.log(x)))
+    .then(forEach(saveAssetsFromLinks))
+    .catch((e) => {
       console.error('[save assets from collections]', e);
     });
 
 
+if (exec) eval(`${exec}()`);
