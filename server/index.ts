7746a260294966d35f906dd053726550a123d9d0
Ivan Flores <hivansan@gmail.com>
2021-10-10 13:57:57 -0500

    dotenv

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,8 @@ import { app, start } from './init';
 const { Client } = require('@elastic/elasticsearch')
 import datasources from '../server/datasources';
 const { es } = datasources;
+import dotenv from 'dotenv';
+dotenv.config();
 
 const db = new Client({ node: es.configuration.node || 'http://localhost:9200' });
 
