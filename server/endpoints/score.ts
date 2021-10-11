acf49cd74e82883d91a10ddd9c16d7da4d58ef77
Ivan Flores <hivansan@gmail.com>
2021-10-11 16:06:18 -0500

    WG-91 rarity endpoint delivering other fields

--- server/endpoints/score.ts
+++ server/endpoints/score.ts
@@ -4,6 +4,7 @@ import { object, string } from '@ailabs/ts-utils/dist/decoder';
 import { match } from '../../models/util';
 import { error, respond } from '../util';
 import * as AssetLoader from '../../lib/asset-loader';
+import { map, pipe, prop } from 'ramda';
 
 const params = {
   getAsset: object('AssetParams', {
@@ -12,45 +13,51 @@ const params = {
   })
 }
 
+const mapTraits = (body: any) =>
+  (pipe(
+    prop('traits') as any,
+    map((t: any) => ({
+      trait_type: t.trait_type,
+      value: t.value,
+      traitStat: t.trait_count / body.count,
+      traitScore: 1 / (t.trait_count / body.count),
+      trait_count: t.trait_count,
+    })) as any
+  ) as any)(body);
+
 export default ({ app, db }: { app: Express, db: ElasticSearch.Client }) => {
 
-  app.get('/api/asset/:contractAddress/:tokenId/score', respond((req) =>
-    params.getAsset(req.params).map(({ contractAddress, tokenId }) =>
-      AssetLoader.assetFromRemote(contractAddress, tokenId)
-        .then((body) => (body === null ? error(404, 'Not found') : (body as any)))
-        .then((body) => ({ traits: body.traits, count: body.collection.stats.count }))
-        .then(({ traits, count }) =>
-          traits.map((t) => ({
-            trait_type: t.trait_type,
-            value: t.value,
-            traitStat: t.trait_count / count,
-            traitScore: 1 / (t.trait_count / count),
-            trait_count: t.trait_count,
-          }))
+
+  app.get(
+    '/api/asset/:contractAddress/:tokenId/score',
+    respond((req) =>
+      params
+        .getAsset(req.params)
+        .map(({ contractAddress, tokenId }) =>
+          AssetLoader.assetFromRemote(contractAddress, tokenId)
+            .then((body) => (body === null ? error(404, 'Not found') : (body as any)))
+            .then((body) => ({ ...body, count: body.collection.stats.count }))
+            .then((body) => ({
+              ...body,
+              traits: mapTraits(body), 
+              ...mapTraits(body).reduce(
+                (acc: { statisticalRarity: number; singleTraitRarity: number; avgTraitRarity: any; rarityScore: number }, t: { traitStat: number }) => ({
+                  statisticalRarity: acc.statisticalRarity * t.traitStat,
+                  singleTraitRarity: Math.min(acc.singleTraitRarity, t.traitStat),
+                  avgTraitRarity: acc.avgTraitRarity + t.traitStat,
+                  rarityScore: acc.rarityScore + 1 / t.traitStat,
+                }),
+                { statisticalRarity: 1, singleTraitRarity: 1, avgTraitRarity: 0, rarityScore: 0, traits: [] }
+              )
+            }))
+            .then((body) => ({ body }))
+            .catch((e) => {
+              console.error('[Get Asset]', e);
+              return error(503, 'Service error');
+            })
         )
-        .then((body) =>
-          body.reduce(
-            (acc: { statisticalRarity: number; singleTraitRarity: number; avgTraitRarity: any; rarityScore: number }, t: { traitStat: number }) => ({
-              statisticalRarity: acc.statisticalRarity * t.traitStat,
-              singleTraitRarity: Math.min(acc.singleTraitRarity, t.traitStat),
-              avgTraitRarity: acc.avgTraitRarity + t.traitStat,
-              rarityScore: acc.rarityScore + 1 / t.traitStat,
-              traits: body,
-            }),
-            { statisticalRarity: 1, singleTraitRarity: 1, avgTraitRarity: 0, rarityScore: 0, traits: [] }
-          )
-        )
-        .then((body) => {
-          console.log(body);
-          return body;
-        })
-        .then((body) => ({ body }))
-        .catch((e) => {
-          console.error('[Get Asset]', e);
-          return error(503, 'Service error');
-        })
+        .defaultTo(error(400, 'Bad request'))
     )
-    .defaultTo(error(400, 'Bad request')))
   );
 
 };
