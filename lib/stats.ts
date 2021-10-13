965c20ea941afee8c3b0fed5849d7919fe2ee3e1
Nate Abele <nate.abele@gmail.com>
2021-10-12 22:43:48 -0400

    Try to fix /score endpoint

---
+++ lib/stats.ts
@@ -0,0 +1,92 @@
+import { curry, path, prop } from "ramda";
+
+import * as Network from './network';
+import { URLSearchParams } from 'url';
+
+export const statsByTraits = curry((traits, count) => {
+  let scores = traits.reduce(
+    (acc, t) => {
+      const norm = t.trait_count / count;
+      acc.traits.push({
+        trait_type: t.trait_type,
+        value: t.value,
+        traitStat: norm,
+        traitScore: 1 / norm,
+      });
+      return {
+        statisticalRarity: acc.statisticalRarity * norm,
+        singleTraitRarity: Math.min(acc.singleTraitRarity, norm),
+        avgTraitRarity: acc.avgTraitRarity + norm,
+        rarityScore: acc.rarityScore + 1 / norm,
+        traits: acc.traits,
+      };
+    },
+    {
+      statisticalRarity: 1,
+      singleTraitRarity: 1,
+      avgTraitRarity: 0,
+      rarityScore: 0,
+      traits: [],
+    }
+  );
+
+  if (scores.avgTraitRarity) {
+    scores.avgTraitRarity /= traits.length;
+  }
+
+  return scores;
+});
+
+export async function collection(contract: string) {
+  const url = 'https://api.opensea.io/api/v1/assets';
+  const params = new URLSearchParams({ asset_contract_address: contract, limit: '1' });
+  const id = await Network.fetchNParse(`${url}?${params}`).then(path(['assets', 0, 'token_id']));
+
+  const asset: any = await Network.fetchNParse(`https://api.opensea.io/api/v1/asset/${contract}/${id}`);
+  const count = asset?.collection?.stats?.count || 0;
+  const limit = 50;
+
+  const iterator: AsyncGenerator<any, any, any> = Network.paginated(
+    result => !result || !(result as any)?.data?.assets?.length,
+    page => {
+      const params = new URLSearchParams({
+        asset_contract_address: contract,
+        offset: (page * limit).toString(),
+        limit: limit.toString(),
+      });
+      return Network.fetchNParse<any>(`${url}?${params}`).then(prop('assets'));
+    }
+  );
+
+  let collectionStats: any[] = [];
+
+  for await (const nfts of iterator) {
+    if (!nfts) {
+      break;
+    }
+    nfts.forEach(nft => {
+      collectionStats.push({
+        ...statsByTraits(nft.traits, count),
+        id: nft.token_id,
+      });
+    });
+  }
+
+  collectionStats
+    .sort((a, b) => a.statisticalRarity - b.statisticalRarity)
+    .forEach((nft, ix) => (nft.statisticalRarityRank = ix + 1));
+
+  collectionStats
+    .sort((a, b) => a.singleTraitRarity - b.singleTraitRarity)
+    .forEach((nft, ix) => (nft.singleTraitRarityRank = ix + 1));
+
+  collectionStats
+    .sort((a, b) => a.avgTraitRarity - b.avgTraitRarity)
+    .forEach((nft, ix) => (nft.avgTraitRarityRank = ix + 1));
+
+  collectionStats
+    .sort((a, b) => b.rarityScore - a.rarityScore)
+    .forEach((nft, ix) => (nft.srarityScoreRank = ix + 1));
+
+  return collectionStats;
+}
--- server/endpoints/score.ts
+++ server/endpoints/score.ts
@@ -1,10 +1,26 @@
+import { any, curry, find, map, mergeRight, objOf, pick, pipe, prop, propEq, tap } from 'ramda';
 import * as ElasticSearch from '@elastic/elasticsearch';
 import { Express } from 'express';
 import { object, string } from '@ailabs/ts-utils/dist/decoder';
 import { match } from '../../models/util';
 import { error, respond } from '../util';
 import * as AssetLoader from '../../lib/asset-loader';
-import { map, pipe, prop } from 'ramda';
+import * as Stats from '../../lib/stats';
+
+type Stats = {
+  statisticalRarity: number;
+  singleTraitRarity: number;
+  avgTraitRarity: number;
+  rarityScore: number;
+};
+
+type Trait = {
+  trait_type: string;
+  value: string | number;
+  trait_count: number;
+}
+
+type TraitStat = { traitStat: number, traitScore: number };
 
 const params = {
   getAsset: object('AssetParams', {
@@ -13,50 +29,62 @@ const params = {
   })
 }
 
-const mapTraits = (body: any) =>
-  (pipe(
-    prop('traits') as any,
-    map((t: any) => ({
-      trait_type: t.trait_type,
-      value: t.value,
-      traitStat: t.trait_count / body.count,
-      traitScore: 1 / (t.trait_count / body.count),
-      trait_count: t.trait_count,
-    })) as any
-  ) as any)(body);
+
+const addProps = <T extends object, U extends object>(fn: (obj: T) => U) => (obj: T) => mergeRight(obj, fn(obj));
+
+const mapTraits = (total: number) => pipe<Trait & {}, Trait, Trait>(
+  pick(['trait_type', 'value', 'trait_count']),
+  addProps<Trait, TraitStat>(({ trait_count }) => ({
+    traitStat: trait_count / total,
+    traitScore: 1 / (trait_count / total),
+  }))
+);
+
+const traitReducer = (acc: Stats & { traits: any[] }, t: { traitStat: number }) => ({
+  statisticalRarity: acc.statisticalRarity * t.traitStat,
+  singleTraitRarity: Math.min(acc.singleTraitRarity, t.traitStat),
+  avgTraitRarity: acc.avgTraitRarity + t.traitStat,
+  rarityScore: acc.rarityScore + 1 / t.traitStat,
+});
 
 export default ({ app, db }: { app: Express, db: ElasticSearch.Client }) => {
 
-
   app.get(
     '/api/asset/:contractAddress/:tokenId/score',
-    respond((req) =>
-      params
-        .getAsset(req.params)
-        .map(({ contractAddress, tokenId }) =>
-          AssetLoader.assetFromRemote(contractAddress, tokenId)
-            .then((body) => (body === null ? error(404, 'Not found') : (body as any)))
-            .then((body) => ({ ...body, count: body.collection.stats.count }))
-            .then((body) => ({
-              ...body,
-              traits: mapTraits(body), 
-              ...mapTraits(body).reduce(
-                (acc: { statisticalRarity: number; singleTraitRarity: number; avgTraitRarity: any; rarityScore: number }, t: { traitStat: number }) => ({
-                  statisticalRarity: acc.statisticalRarity * t.traitStat,
-                  singleTraitRarity: Math.min(acc.singleTraitRarity, t.traitStat),
-                  avgTraitRarity: acc.avgTraitRarity + t.traitStat,
-                  rarityScore: acc.rarityScore + 1 / t.traitStat,
-                }),
-                { statisticalRarity: 1, singleTraitRarity: 1, avgTraitRarity: 0, rarityScore: 0, traits: [] }
-              )
-            }))
-            .then((body) => ({ body }))
-            .catch((e) => {
-              console.error('[Get Asset]', e);
-              return error(503, 'Service error');
-            })
-        )
-        .defaultTo(error(400, 'Bad request'))
+    respond((req) => params.getAsset(req.params).map(({ contractAddress, tokenId }) => {
+      return Promise.all([
+        AssetLoader.assetFromRemote(contractAddress, tokenId),
+        Stats.collection(contractAddress).then(find(propEq('id', tokenId)))
+      ])
+        .then(([body, stats]: [any, any]) => {
+          const count = body?.collection?.stats?.count || null;
+          const mapped = body?.traits?.map(mapTraits(count)) || [];
+          console.log('STATS', stats);
+
+          return (
+            body === null
+              ? error(404, 'Not found') as any
+              : {
+                body: mergeRight(body, {
+                  count,
+                  traits: mapped,
+                  ...mapped.reduce(traitReducer, {
+                    statisticalRarity: 1,
+                    singleTraitRarity: 1,
+                    avgTraitRarity: 0,
+                    rarityScore: 0,
+                    traits: []
+                  })
+                })
+              }
+          )
+        })
+        .catch((e) => {
+          console.error('[Get Asset]', e);
+          return error(503, 'Service error');
+        })
+    })
+      .defaultTo(error(400, 'Bad request'))
     )
   );
 
