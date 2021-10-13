8bdba60f2470a8a269e8b4db5c69d6225bb4752c
Nate Abele <nate.abele@gmail.com>
2021-10-12 22:43:12 -0400

    Catch-all 404 route

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import { app, start } from './init';
 import datasources from '../server/datasources';
 import { Client } from '@elastic/elasticsearch';
+import { respond } from './util';
 const { es } = datasources;
 
 const db = new Client({ node: es.configuration.node || 'http://localhost:9200' });
@@ -14,5 +15,13 @@ const routes = [
 ];
 
 routes.forEach(name => require(`./endpoints/${name}`).default({ app, db }));
+
+/**
+ * Catch-all 404 to replace Express default handler
+ */
+app.get('/*', respond(() => ({
+  status: 404,
+  body: { msg: 'Not found' }
+})));
 
 start();
