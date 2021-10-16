75f232b94f345a0e4c51e2f272985f412834aed9
Nate Abele <nate.abele@gmail.com>
2021-10-16 00:10:08 -0400

    Rewrite stats function to take data from DB

--- lib/stats.ts
+++ lib/stats.ts
@@ -1,34 +1,30 @@
-import { curry, path, prop } from "ramda";
-
-import * as Network from './network';
-import { URLSearchParams } from 'url';
+import { curry } from "ramda";
+import { CollectionStats } from "../models/collection";
+import { Asset } from "../models/asset";
 
 export const statsByTraits = curry((traits, count) => {
-  let scores = traits.reduce(
-    (acc, t) => {
-      const norm = t.trait_count / count;
-      acc.traits.push({
+  let scores = traits.reduce((acc, t) => {
+    const norm = t.trait_count / count;
+
+    return {
+      statisticalRarity: acc.statisticalRarity * norm,
+      singleTraitRarity: Math.min(acc.singleTraitRarity, norm),
+      avgTraitRarity: acc.avgTraitRarity + norm,
+      rarityScore: acc.rarityScore + 1 / norm,
+      traits: acc.traits.concat([{
         trait_type: t.trait_type,
         value: t.value,
         traitStat: norm,
         traitScore: 1 / norm,
-      });
-      return {
-        statisticalRarity: acc.statisticalRarity * norm,
-        singleTraitRarity: Math.min(acc.singleTraitRarity, norm),
-        avgTraitRarity: acc.avgTraitRarity + norm,
-        rarityScore: acc.rarityScore + 1 / norm,
-        traits: acc.traits,
-      };
-    },
-    {
-      statisticalRarity: 1,
-      singleTraitRarity: 1,
-      avgTraitRarity: 0,
-      rarityScore: 0,
-      traits: [],
-    }
-  );
+      }]),
+    };
+  }, {
+    statisticalRarity: 1,
+    singleTraitRarity: 1,
+    avgTraitRarity: 0,
+    rarityScore: 0,
+    traits: [],
+  });
 
   if (scores.avgTraitRarity) {
     scores.avgTraitRarity /= traits.length;
@@ -37,40 +33,11 @@ export const statsByTraits = curry((traits, count) => {
   return scores;
 });
 
-export async function collection(contract: string) {
-  const url = 'https://api.opensea.io/api/v1/assets';
-  const params = new URLSearchParams({ asset_contract_address: contract, limit: '1' });
-  const id = await Network.fetchNParse(`${url}?${params}`).then(path(['assets', 0, 'token_id']));
-
-  const asset: any = await Network.fetchNParse(`https://api.opensea.io/api/v1/asset/${contract}/${id}`);
-  const count = asset?.collection?.stats?.count || 0;
-  const limit = 50;
-
-  const iterator: AsyncGenerator<any, any, any> = Network.paginated(
-    result => !result || !result.length || result.length < limit,
-    page => {
-      const params = new URLSearchParams({
-        asset_contract_address: contract,
-        offset: (page * limit).toString(),
-        limit: limit.toString(),
-      });
-      return Network.fetchNParse<any>(`${url}?${params}`).then(prop('assets'));
-    }
-  );
-
-  let collectionStats: any[] = [];
-
-  for await (const nfts of iterator) {
-    if (!nfts) {
-      break;
-    }
-    nfts.forEach(nft => {
-      collectionStats.push({
-        ...statsByTraits(nft.traits, count),
-        id: nft.token_id,
-      });
-    });
-  }
+export async function collection(collection: CollectionStats, assets: Asset[]) {
+  const collectionStats = assets.map(asset => Object.assign(
+    { id: asset.tokenId },
+    statsByTraits(asset.traits, collection.count)
+  ));
 
   collectionStats
     .sort((a, b) => a.statisticalRarity - b.statisticalRarity)
