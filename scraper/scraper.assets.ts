8436c9eda32efb55ea0ac3eea2eadf1276fe3208
Ivan <hivansan@gmail.com>
2021-10-26 19:04:46 -0500

    scraper promise-retry

--- scraper/scraper.assets.ts
+++ scraper/scraper.assets.ts
@@ -13,10 +13,11 @@
  */
 
 import fs from 'fs';
-import moment from 'moment';
+// import moment from 'moment';
 import axios from 'axios';
 
 import { map, pick, pipe, toString, prop, props, sortBy, tap, flatten, dropRepeats, split, forEach, filter, mergeRight, path, clamp, ifElse } from 'ramda';
+
 import { sleep } from '../server/util';
 import { load, openseaAssetMapper, readPromise } from './scraper.utils';
 import * as Query from '../lib/query';
@@ -24,6 +25,7 @@ import { Client } from '@elastic/elasticsearch';
 import { toResult } from '../server/endpoints/util';
 import datasources from '../server/datasources';
 
+const promiseRetry = require('promise-retry');
 const torAxios = require('tor-axios');
 import * as AssetLoader from '../lib/asset-loader';
 
@@ -38,12 +40,14 @@ const bail = (err: any) => {
 };
 
 const exec: string | undefined = process.argv.find((s) => s.startsWith('--exec='))?.replace('--exec=', '');
-const bots: number = +(process.argv.find((s) => s.startsWith('--bots='))?.replace('--bots=', '') as any);
+const bots: number = +(process.argv.find((s) => s.startsWith('--bots='))?.replace('--bots=', '') as any) || 1;
 const errsToFile: any = process.argv.find((s) => s.startsWith('--errsToFile='))?.replace('--errsToFile=', '') || './data/errors-to.txt';
 const errsFromFile: any = process.argv.find((s) => s.startsWith('--errsFromFile='))?.replace('--errsFromFile=', '') || './data/errors-from.txt';
 const collectionFilter: string = process.argv.find((s) => s.startsWith('--collectionFilter='))?.replace('--collectionFilter=', '') || '';
 
 const DAYS_WINDOW = 5;
+
+let collectionsCounts: any = {};
 
 const dum = (data: unknown): any => // just to not break the previous flow
   new Promise((resolve, reject) => {
@@ -66,10 +70,11 @@ export const saveAssetsFromLinks = async (links: string[], i?: number): Promise<
   for (const url of links) {
     const clientCall = !!bots ? tor.get(url) : axios(url);
     // console.log(!!bots, bots, url);
-    await sleep(1.35);
-    try {
+    await sleep(0.35);
       // const data = await clientCall;
-      clientCall// dum(data)
+    // try {
+    promiseRetry({ retries: 5 }, (retry: any, number: any) =>
+      clientCall
         .then(({ data }: any) => ({
           assets: data.assets,
           slug: (url as any)
@@ -81,52 +86,45 @@ export const saveAssetsFromLinks = async (links: string[], i?: number): Promise<
             .find((s: string) => s.startsWith('offset='))
             .split('=')[1],
         }))
-        // .then(tap((x) => console.log(x)))
         .then((body: { slug: any }) => ({ ...body, filteredBySlug: links.filter((s) => s.includes(`collection=${body.slug}`)) }))
-        .then(tap(({ assets, slug, offset }: any) => console.log(url, assets?.length, `-- ${i}`)))
+        .then(tap(({ assets, slug, offset }: any) => console.log(`[url] ${url} assets: ${assets.length} thread: ${i} offset: ${offset}`)))
         .then(({ assets, slug, offset, filteredBySlug }: any) => {
+          const isLastUrlOfCollection = (+offset + 50) > +collectionsCounts[slug].supply; // links.indexOf(url) == filteredBySlug.length - 1;
+
+          console.log('collectionsCounts --', collectionsCounts);
+          
+          
           if (assets?.length) {
             const content = JSON.stringify(assets.map(openseaAssetMapper)) as any;
             load(JSON.parse(content), 'assets');
-
-            const isLastUrlOfCollection = links.indexOf(url) == filteredBySlug.length - 1;
-            // console.log(`[${slug}] isLastUrlOfCollection: ${isLastUrlOfCollection} length: ${filteredBySlug.length - 1}`);
-
             if (isLastUrlOfCollection || assets.length < 50) {
               Query.update(db, 'collections', slug, { updatedAt: new Date() }, true).catch((e) => console.log(`[err update collection] url: ${url} ${e}`));
             }
           } else {
             Query.update(db, 'collections', slug, { updatedAt: new Date() }, true).catch((e) => console.log(`[err update collection] url: ${url} ${e}`));
           }
+          return isLastUrlOfCollection;
         })
         .catch(async (e: any) => {
+          console.log(`[err inside PR] ${e} ${url}`);
           await tor.torNewSession();
-          console.log(`[err] ${e} ${url}`);
-          fs.appendFile(errsToFile, `${url}\n`, (err) => {
-            if (err) console.log(`[write 504 file error]`, err);
-          });
-        });
-    } catch (e) {
-      await tor.torNewSession();
-      console.log(`[err] ${e} ${url}`);
-      fs.appendFile(errsToFile, `${url}\n`, (err) => {
-        if (err) console.log(`[write 504 file error]`, err);
+          retry(e);
+        })
+    )
+      .then((result: any) => {
+        console.log('isLastUrlOfCollection --', result);
+        return result;
+      })
+      .catch(async (e: any) => {
+        console.log(`[err catch PR] ${e} ${url}`);
+        // fs.appendFile(errsToFile, `${url}\n`, (err) => {
+        //   if (err) console.log(`[write 504 file error]`, err);
+        // });
       });
-    }
   }
 };
 
-const getLinks = (c: { totalSupply: number; slug: any }): string[] =>
-  [...Array(Math.ceil(c.totalSupply / 50)).keys()].map((i) =>
-    `https://api.opensea.io/api/v1/assets?format=json&limit=50&offset=${i * 50}&collection=${c.slug}`);
-
 const topSupply = map((c) => ({ ...Object(c), totalSupply: Object(c).totalSupply > 10000 ? 10000 : Object(c).totalSupply }));
-const toLinks = map((c): string[] => [...getLinks(Object(c))]);
-
-const getSplices = (links: string[]): string[][] => {
-  const len = Math.ceil(links.length / +bots);
-  return [...Array(+bots).keys()].map((c, i) => links.splice(0, len));
-};
 
 /** this only saves the array of links sliced. optionally further to read from several instances */
 const saveLinkSlicedFile = (links: string[][]) => {
@@ -147,14 +145,25 @@ const filterData = pipe(
   filter((c: any) => c.totalSupply),
 );
 
+const getLinks = (c: { totalSupply: number; slug: any }): string[] =>
+  [...Array(Math.ceil(c.totalSupply / 50)).keys()].map((i) =>
+    `https://api.opensea.io/api/v1/assets?format=json&limit=50&offset=${i * 50}&collection=${c.slug}`);
+
+const toLinks = map((c): string[] => [...getLinks(Object(c))]);
+
+const getSplices = (links: string[]): string[][] => {
+  const len = Math.ceil(links.length / +bots);
+  return [...Array(+bots).keys()].map((c, i) => links.splice(0, len));
+};
+
 const divideLinksByWorkers = pipe(toLinks, flatten, dropRepeats, getSplices);
 
 /**
  * arrays from links and send them to download to tor or axios
  */
-const distributeToHttpClients = (arrOfLinks: string[][]) => {
+const distributeToHttpClients = tap((arrOfLinks: string[][]) => {
   for (const [i, links] of arrOfLinks.entries()) saveAssetsFromLinks(links, i);
-};
+});
 
 const collectionData = (slug?: string) =>
   !!slug
@@ -167,7 +176,7 @@ const collectionData = (slug?: string) =>
         body.results.filter((c: { slug: string | any[] }) => c.slug?.length)
     );
 
-const countInDb = (collections: any[]) => {
+export const countInDb = (collections: any[]): any => {
   const dbPromises = collections.map((c: { slug: any }) => Query.count(db, 'assets', { match: { slug: c.slug } }, {}));
     return Promise.all(dbPromises)
       .then((dbResults: any[]) =>
@@ -184,6 +193,8 @@ const countInDb = (collections: any[]) => {
       .catch((e) => console.log(`[err], ${e}`));
 }
 
+const assignSupplies = (x: any[]) => (collectionsCounts = x.reduce((obj, cur, i) => ((obj[cur.slug] = { supply: cur.totalSupply }), obj), {}));
+
 export const saveAssets = (slug?: string) =>
   collectionData(slug)
     .then(countInDb as any)
@@ -191,6 +202,7 @@ export const saveAssets = (slug?: string) =>
     .then(filter((c: any) => c.shouldScrape))
     .then(topSupply as any)
     .then(filterData as any)
+    .then(tap(assignSupplies) as any)
     .then(divideLinksByWorkers as any)
     // .then(saveLinkSlicedFile) // optional
     .then(distributeToHttpClients as any)
