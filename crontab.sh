ddf8349a5c8effaf6363492ec8210b07d9f59a37
Ivan <hivansan@gmail.com>
2021-11-05 13:23:00 -0600

    crontab and dev commands

--- crontab.sh
+++ crontab.sh
@@ -1,2 +1,2 @@
-* * * * * ES_CLIENT=https://vpc-wego-db-1-mz5tglq65ulqsc5p52nf7gy2ke.us-east-1.es.amazonaws.com:443 /usr/bin/env node /home/ubuntu/api/dist/scraper/scraper.assets.js --exec=saveAssets --onlyRequested=1 --linear=true --factor=.4 --errsToFile=/home/ubuntu/data/errors-to-assets.txt &>/tmp/mycommand.log
+*/5 * * * * ES_CLIENT=https://vpc-wego-db-1-mz5tglq65ulqsc5p52nf7gy2ke.us-east-1.es.amazonaws.com:443 /usr/bin/env node /home/ubuntu/api/dist/scraper/scraper.assets.js --exec=saveAssets --onlyRequested=1 --linear=true --factor=3 --errsToFile=/home/ubuntu/data/errors-to-assets.txt &>/tmp/mycommand.log
 * * * * * ES_CLIENT=https://vpc-wego-db-1-mz5tglq65ulqsc5p52nf7gy2ke.us-east-1.es.amazonaws.com:443 /usr/bin/env node /home/ubuntu/api/dist/scraper/rank.js &>/tmp/mycommand.log
