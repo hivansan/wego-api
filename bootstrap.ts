350e40fed7874c3afbbdd406574932cf27cb699a
Ivan <hivansan@gmail.com>
2021-11-04 20:51:38 -0600

    tsconfig include scraper

--- bootstrap.ts
+++ bootstrap.ts
@@ -3,4 +3,8 @@ import { Client } from '@elastic/elasticsearch';
 import datasources from './server/datasources';
 const { es } = datasources;
 
-export const db = new Client({ node: es.configuration.node || 'http://localhost:9200' });
+export const db = new Client({
+  node: es.configuration.node || 'http://localhost:9200', ssl: {
+    rejectUnauthorized: false,
+  }
+});
