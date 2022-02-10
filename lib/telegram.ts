83426b5ea0d3bef9990ed861385de8caf38484e1
dh-mauricio <mauricio@digitalhedge.com>
2022-02-10 07:59:55 -0600

    WG-229 save telegram-users

--- lib/telegram.ts
+++ lib/telegram.ts
@@ -1,51 +1,48 @@
 'use strict';
 
 import axios from 'axios';
-import { any, flatten, map, path, pipe, prop } from 'ramda';
+import { flatten, map, path, pipe, prop, tap } from 'ramda';
 import * as Query from './query';
 import { db } from '../bootstrap';
 import { toResult } from '../server/endpoints/util';
 import dotenv from 'dotenv';
+import { load } from '../scraper/scraper.utils';
 
 dotenv.config();
 const baseURL = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`;
 
-export default class TelegramUtils {
+const main = async () =>
+  Query.find(db, 'collections', {
+    'bool': {
+      'must': [
+        { 'exists': { 'field': 'telegram' } }
+      ]
+    }
+  },
+    { limit: 5000, source: ['slug', 'telegram', 'stats'] })
+    .then(pipe<any, any, any, any>(
+      path(['body', 'hits', 'hits']),
+      map(pipe(toResult, prop('value'))) as unknown as (v: any) => any[],
+      (collections) => {
+        if (!collections?.length) return;
+        return Promise.all(collections.map(async collection => {
+          const chat_id = collection.telegram.split('/').pop();
 
-  async getUsersByChannel() {
-    const collections = await Query.find(db, 'collections', {
-      'bool': {
-        'must': [
-          { 'exists': { 'field': 'telegram' } }
-        ]
-      }
-    },
-      { limit: 15000, source: ['slug', 'telegram'] })
-      .then(pipe<any, any, any, any>(
-        path(['body', 'hits', 'hits']),
-        map(pipe(toResult, prop('value'))) as unknown as (v: any) => any[],
-        async (collections) => {
-          if (!collections?.length) return;
-          return Promise.all(collections.map(async collection => {
-            const chat_id = collection.telegram.split('/').pop();
-            const telegramRes = await axios.get(baseURL + `/getChatMembersCount?chat_id=@${chat_id}`)
-              .then(result => result.data)
-            return {
-              ...collection,
-              telegram_users: telegramRes.result
-            }
-          }))
-        }
-      ))
-      .catch(e => {
-        console.log(e);
-      });
-    return collections;
-  }
-}
+          return await axios.get(baseURL + `/getChatMembersCount?chat_id=@${chat_id}`)
+            .then(prop('data'))
+            .catch(e => {
+              console.log(`[error axios telegram]`, `${e.response.data.error_code}-${e.response.data.description}`);
+            })
+            .then(telegramObj => {
+              return {
+                ...collection,
+                stats: { ...collection.stats, telegram_users: telegramObj?.result || 0 }
+              }
+            })
+        }))
+      }))
+    .then(flatten)
+    .then(tap(collections => load(collections, 'collections', 'upsert')))
 
-const main = async () => {
-  const telegram = new TelegramUtils();
-  console.log(await telegram.getUsersByChannel());
-};
+
 if (require.main === module) main();
