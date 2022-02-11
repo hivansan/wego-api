120311c9ee3b90496e0bb755fe93f539b4915eae
Ivan <hivansan@gmail.com>
2022-02-11 13:00:03 -0600

    deploy scraper sh

--- bin/deploy-scraper.sh
+++ bin/deploy-scraper.sh
@@ -3,7 +3,15 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 DIST_DIR="${SCRIPT_DIR}/../dist"
 
+HOST1="${HOST1:-wego-scraper}"
+HOST2="${HOST2:-wego-scraper-10k}"
+
 # ssh -t wego-scraper "cd api && git pull && npm run build"
 npm run build 
-rsync -azP ${DIST_DIR}/ wego-scraper:/home/ubuntu/api/ --exclude public
-rsync -azP ${DIST_DIR}/ wego-scraper-10k:/home/ubuntu/api/ --exclude public
+cp ./*.json ${DIST_DIR}
+rsync -azP ${DIST_DIR}/ ${HOST1}:/home/ubuntu/api/ --exclude public
+rsync -azP ${DIST_DIR}/ ${HOST2}:/home/ubuntu/api/ --exclude public
+
+
+ssh ${HOST1} "cd ~/api; npm i;"
+ssh ${HOST2} "cd ~/api; npm i;"
