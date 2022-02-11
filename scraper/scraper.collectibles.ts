4a5ccba944af5bbc37b5fe21a841e830ea1587d2
Ivan <hivansan@gmail.com>
2022-02-11 12:42:19 -0600

    WG-232 scrape collections save to db

--- scraper/scraper.collectibles.ts
+++ scraper/scraper.collectibles.ts
@@ -1,62 +1,76 @@
-import { filter, map } from 'ramda';
+import { difference, filter, map, objOf, path, pipe, prop, tap, uniq } from 'ramda';
 
-const puppeteer = require('puppeteer-extra');
+import puppeteer from 'puppeteer-extra';
 
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { Page } from 'puppeteer';
+
+import * as Query from '../lib/query';
+import { db } from '../bootstrap';
+import { toResult } from '../server/endpoints/util';
+import { load } from './scraper.utils';
+
+
+
 puppeteer.use(StealthPlugin());
 const debug = true;
-let collections: any[] = [];
 
 const main = async () => {
   const browser = await puppeteer.launch({
     headless: !debug
   });
   const page = await browser.newPage();
-  const url = "https://opensea.io/rankings?category=collectibles&chain=ethereum&sortBy=thirty_day_volume";
+  const url = 'https://opensea.io/rankings?category=collectibles&chain=ethereum&sortBy=thirty_day_volume';
   let hasNextPage = true;
   await page.goto(url);
   await page.waitForSelector('.cf-browser-verification', { hidden: true });
 
   while (hasNextPage) {
     await page.waitForSelector('.Image--image');
-    await (scrapePage(page));
+    const _slugs = await (scrapePage(page));
+    // slugs = slugs.concat(_slugs)
+
+    Query.find(db, 'collections', { terms: { _id: _slugs } }, { limit: _slugs.length, source: ['slug'] })
+      .then(path(['body', 'hits', 'hits']) as any)
+      .then(map(pipe(toResult, prop('value'))))
+      .then(map(prop('slug')) as any)
+      .then(difference(_slugs) as any)
+      .then(map(objOf('slug')))
+      .then(tap(newCollections => {
+        // console.log('newCollections -------', newCollections)
+        load(newCollections, 'collections', 'upsert')
+      }))
+
     const btn_next = await page.$$('.Buttonreact__StyledButton-sc-glfma3-0');
-    const lastPage = await (await btn_next[1].getProperty("disabled")).jsonValue();
+    const lastPage = await (await btn_next[1].getProperty('disabled')).jsonValue();
     hasNextPage = !lastPage;
     if (hasNextPage) {
       await btn_next[1].click();
     }
   }
-  console.log(collections);
+
   await browser.close();
 };
 
-async function scrapePage(page) {
-
+async function scrapePage(page: Page) {
   const scrollHeight = await page.evaluate('document.body.scrollHeight');
   let currentHeight = 0;
-  const collections_temp: any[] = [];
+  let slugs = []
   while (currentHeight < scrollHeight) {
     await page.evaluate('window.scrollBy(0, 200)');
-    await page.$$eval("a", links => {
-      return links.map(link => link.href)
-    })
-      .then(filter((link: any) => link.includes("https://opensea.io/collection/")))
-      .then(map((slug: any) => {
-        const _slug = slug.split("/").pop()
-        if (!collections_temp[_slug]) {
-          collections_temp[_slug] = {
-            slug: _slug
-          }
-          return _slug;
-        }
-      }))
-      .then(filter((slug: any) => slug !== undefined))
-      .then(map((slug: any) => {
-        collections.push(collections_temp[slug])
-      }))
+
+    await page.$$eval('a', urls => urls.map(url => url.href))
+      .then(filter((url: any) => url.includes('https://opensea.io/collection/')))
+      .then(map((url: any) => url.split('/').pop()))
+      .then(filter((slug: any) => !!slug))
+      .then(uniq)
+      .then(tap((_slugs: any) => { slugs = slugs.concat(_slugs) }))
+
     currentHeight += 100;
   }
+
+  return uniq(slugs);
+
 }
 
 if (require.main === module) main();
--- scraper/scraper.collections.ts
+++ scraper/scraper.collections.ts
@@ -11,6 +11,7 @@ import { toResult } from '../server/endpoints/util';
 import * as AssetLoader from '../lib/asset-loader';
 import { db } from '../bootstrap';
 import * as Historicals from '../lib/historicals.utils';
+import { map, path, pipe, prop } from 'ramda';
 
 const main = () => {
   const q = { match_all: {} };
@@ -18,25 +19,31 @@ const main = () => {
   // const q = { bool: { must_not: { match: { 'deleted': true } } } };
   // const q = { bool: { must: { match: { 'slug.keyword': 'boredapeyachtclub' } } } };
 
-  Query.find(db, 'collections', q, { limit: 5000, sort: [{ 'stats.totalVolume': { order: 'desc' } }], source: ['slug', 'deleted', 'stats.totalVolume', 'stats.featuredCollection'] })
-    .then(
-      ({ body: { took, timed_out: timedOut, hits: { total, hits }, }, }) =>
-        ({ body: { meta: { took, timedOut, total: total.value }, results: hits.map(toResult).map((r: { value: any; }) => r.value), }, })
-    )
-    .then(async (fromDB) => {
-
-      const collections = fromDB.body.results
+  Query.find(db, 'collections', q,
+    {
+      limit: 5000,
+      sort: [{ 'stats.totalVolume': { order: 'desc', missing: '_first' } }],
+      source: ['slug', 'deleted', 'stats.totalVolume', 'stats.featuredCollection']
+    }
+  )
+    // .then(
+    //   ({ body: { took, timed_out: timedOut, hits: { total, hits }, }, }) =>
+    //     ({ body: { meta: { took, timedOut, total: total.value }, results: hits.map(toResult).map((r: { value: any; }) => r.value), }, })
+    // )
+    .then(path(['body', 'hits', 'hits']) as any)
+    .then(map(pipe(toResult, prop('value'))))
+    .then(async (collections) => {
       for (const collection of collections) {
         await sleep(0.8);
         AssetLoader.collectionFromRemote(collection.slug)
           // colection from remote
           .then(async (collectionFR) => {
-            if (collectionFR !== null) {
+            if (collectionFR !== null/*  && !!collection.stats */) {
               Historicals.load({ index: 'collections', id: collectionFR.slug, slug: collectionFR.slug, data: collectionFR.stats })
-              console.log(`${collection.slug}, deleted: ${collectionFR.deleted} \t\t actual Volume: ${collection.stats.totalVolume} os: ${collectionFR.stats.totalVolume}`);
+              console.log(`${collection.slug}, deleted: ${collectionFR.deleted} \t\t actual Volume: ${collection.stats?.totalVolume} os: ${collectionFR.stats.totalVolume}`);
               (collectionFR as any).stats.featuredCollection = collection.stats?.featuredCollection || false;
               try {
-                if (collection.stats.totalVolume === collectionFR.stats.totalVolume) return;
+                if (collection.stats?.totalVolume === collectionFR.stats.totalVolume) return;
                 console.log(`updating ${collection.slug}`);
                 await Query.update(db, 'collections', collection.slug, collectionFR, true);
                 console.log(`[success updated collection] ${collectionFR.slug}`);
@@ -45,10 +52,19 @@ const main = () => {
                 await Query.updateByQuery(db, 'assets', { match: { 'slug.keyword': collection.slug } }, { source: `ctx._source['deleted'] = ${collectionFR.deleted}` }, true);
                 console.log(`[success updated assets] ${collectionFR.slug}`);
               } catch (error) {
-                console.log('error in ', collection.slug, error?.meta?.body ? JSON.stringify(error.meta.body, null, 2) : error);
+                console.log('[error in collection scraper] ', collection.slug, error?.meta?.body ? JSON.stringify(error.meta.body, null, 2) : error);
               }
 
-            }
+            } /* else if (collectionFR !== null && !collection.stats) {
+              try {
+                console.log('collectionFR', collectionFR);
+
+                await Query.update(db, 'collections', collection.slug, collectionFR, true);
+
+              } catch (error) {
+                console.log('[error in collection scraper] ', collection.slug, error?.meta?.body ? JSON.stringify(error.meta.body, null, 2) : error);
+              }
+            } */
           });
       }
     });
