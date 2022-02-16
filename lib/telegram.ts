33fddfbae8c2c74ca996556998945476c013b7d2
dh-mauricio <mauricio@digitalhedge.com>
2022-02-16 14:45:44 -0600

    WG-229 change requests loop

--- lib/telegram.ts
+++ lib/telegram.ts
@@ -1,12 +1,13 @@
 'use strict';
 
 import axios from 'axios';
-import { flatten, map, path, pipe, prop, tap } from 'ramda';
+import { map, path, pipe, prop } from 'ramda';
 import * as Query from './query';
 import { db } from '../bootstrap';
 import { toResult } from '../server/endpoints/util';
 import dotenv from 'dotenv';
 import { load } from '../scraper/scraper.utils';
+import { sleep } from '../server/util';
 
 dotenv.config();
 const baseURL = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`;
@@ -23,26 +24,29 @@ const main = async () =>
     .then(pipe<any, any, any, any>(
       path(['body', 'hits', 'hits']),
       map(pipe(toResult, prop('value'))) as unknown as (v: any) => any[],
-      (collections) => {
+      async (collections) => {
         if (!collections?.length) return;
-        return Promise.all(collections.map(async collection => {
+        const telegramCollections: any = [];
+        let counter = 0;
+        for (const collection of collections) {
           const chat_id = collection.telegram.split('/').pop();
-
-          return await axios.get(baseURL + `/getChatMembersCount?chat_id=@${chat_id}`)
+          await axios.get(baseURL + `/getChatMembersCount?chat_id=@${chat_id}`)
             .then(prop('data'))
             .catch(e => {
               console.log(`[error axios telegram]`, `${e.response.data.error_code}-${e.response.data.description}`);
             })
-            .then(telegramObj => {
-              return {
+            .then((telegramObj: any) => {
+              telegramCollections.push({
                 ...collection,
                 stats: { ...collection.stats, telegram_users: telegramObj?.result || 0 }
-              }
+              })
             })
-        }))
-      }))
-    .then(flatten)
-    .then(tap(collections => load(collections, 'collections', 'upsert')))
+          counter++;
+          counter % 20 === 0 ? await sleep(60) : await sleep(1);
+        }
+        load(telegramCollections, 'collections', 'upsert')
+      },
+    ))
 
 
 if (require.main === module) main();
